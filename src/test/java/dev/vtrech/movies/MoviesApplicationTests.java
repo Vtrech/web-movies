b585f9e563ed41d2092d441f05794fbedc935bfd
Vtrech <vdtruong1501@gmail.com>
2025-07-03 16:03:08 +0700

    initial

---
+++ src/test/java/dev/vtrech/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vtrech.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
